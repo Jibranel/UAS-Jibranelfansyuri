eea0692b69812df724f7c5b188c226a3aa9db585
Jibranel <101688324+Jibranel@users.noreply.github.com>
2022-07-09 00:07:52 +0700

    UAS

---
+++ app/src/main/java/com/jibran/uts_jibranelfansyuri/MainActivity.java
@@ -0,0 +1,28 @@
+package com.jibran.uts_jibranelfansyuri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Login= (Button) findViewById(R.id.login);
+
+        Login.setOnClickListener(v -> {
+            Intent intent1 = new Intent(getApplicationContext(),MenuUtama.class);
+            startActivity(intent1);
+
+        });
+    }
+}
---
+++ app/src/main/java/com/jibran/uts_jibranelfansyuri/MenuUtama.java
@@ -0,0 +1,92 @@
+package com.jibran.uts_jibranelfansyuri;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuUtama extends AppCompatActivity {
+    Button button1, button2, button3, button4, button5, button6, button7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_utama);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuUtama.this, WisataAlam.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuUtama.this, WisataEdukasi.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuUtama.this, WisataReligi.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuUtama.this, WisataKuliner.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuUtama.this, About.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                moveTaskToBack(true);
+
+                //membuat method tombol keluar dari aplikasi
+
+            }
+
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuUtama.this, SmsSend.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+    }
+}
